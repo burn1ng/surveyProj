59698fafd7d859933d4426a695fe4afd539335a3
burn1ng <andrew.burn1ng@gmail.com>
2017-07-12 13:54:58 +0300

    about page is finished

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	console.log("Document ready!");
+    console.log("Document ready!");
+
+    $('#about-page-tabs a').click(function(e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
 });
