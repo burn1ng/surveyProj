d8f9d816585b2af77e2e3aae9eed57cfbb9613b8
burn1ng <andrew.burn1ng@gmail.com>
2017-07-13 16:37:39 +0300

    changed all logic of page layout

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -4,5 +4,16 @@ $(document).ready(function() {
     $('#about-page-tabs a').click(function(e) {
         e.preventDefault()
         $(this).tab('show')
-    })
+    });
+
+    // $("#menu-toggle").click(function(e) {
+    //     e.preventDefault();
+    //     $(body).toggleClass("toggled");
+    // });
+
+    // if($(document).height() <= $(window).height())
+    //     $("footer.footer").addClass("navbar-fixed-bottom");
+
+    // $('.page-wrapper').addClass("toggled");
+
 });
